87970d1fe69e973b5c08420bea27d1d0467a223c
SebaDro <sebastian.drost@hs-bochum.de>
2019-05-07 09:20:12 +0200

    Add encoder and decoder

---
+++ dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdProductsMetadataDecoder.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wacodis.dwd.cdc;
+
+/**
+ * Decodes DWD products metadata into a DataEnvelope
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public class DwdProductsMetadataDecoder {
+
+}
---
+++ dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdRequestParamsEncoder.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.wacodis.dwd.cdc;
+
+/**
+ * Encodes a SubsetDefinition into request paramateres for a DWD WFS request
+ *
+ * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
+ */
+public class DwdRequestParamsEncoder {
+
+}
