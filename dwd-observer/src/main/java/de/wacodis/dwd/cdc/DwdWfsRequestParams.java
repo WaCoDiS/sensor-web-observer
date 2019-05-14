be14e125913767b67bef70273829840fcc8f3917
Tim <tim.kurowski@hs-bochum.de>
2019-05-14 10:03:13 +0200

    create parameters

--- dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdWfsRequestParams.java
+++ dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdWfsRequestParams.java
@@ -5,6 +5,10 @@
  */
 package de.wacodis.dwd.cdc;
 
+import java.util.Date;
+
+import org.geotools.geometry.Envelope2D;
+
 /**
  * Encapuslates parameters for requesting DWD WFS services
  *
@@ -12,6 +16,68 @@ package de.wacodis.dwd.cdc;
  */
 public class DwdWfsRequestParams {
 
-    //TODO: define additional required requests parameters
-	
+	// https://cdc.dwd.de:443/geoserver/CDC/wfs? +
+
+	String version;
+	String typeName;
+	Envelope2D bbox;
+	Date startDate;
+	Date endDate;
+
+	// Konstruktor
+	public DwdWfsRequestParams() {
+
+	}
+
+	public DwdWfsRequestParams(String version, String typeName, Envelope2D bbox, Date startDate, Date endDate) {
+		super();
+		this.version = version;
+		this.typeName = typeName;
+		this.bbox = bbox;
+		this.startDate = startDate;
+		this.endDate = endDate;
+	}
+
+	// Getters and Setters
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getTypeName() {
+		return typeName;
+	}
+
+	public void setTypeName(String typeName) {
+		this.typeName = typeName;
+	}
+
+	public Envelope2D getBbox() {
+		return bbox;
+	}
+
+	public void setBbox(Envelope2D bounds) {
+		this.bbox = bounds;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return startDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
 }
