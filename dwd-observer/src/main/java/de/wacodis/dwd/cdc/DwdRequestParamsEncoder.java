8b8583e9865eb0a1ca6a26fcb9c3542abd0280d7
Tim <tim.kurowski@hs-bochum.de>
2019-05-14 10:03:34 +0200

    create parameters encoder

--- dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdRequestParamsEncoder.java
+++ dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdRequestParamsEncoder.java
@@ -5,6 +5,13 @@
  */
 package de.wacodis.dwd.cdc;
 
+import java.util.Date;
+import java.util.List;
+
+import org.geotools.geometry.DirectPosition2D;
+import org.geotools.geometry.Envelope2D;
+
+import de.wacodis.api.model.AbstractDataEnvelopeAreaOfInterest;
 import de.wacodis.api.model.WacodisJobDefinition;
 
 /**
@@ -14,11 +21,25 @@ import de.wacodis.api.model.WacodisJobDefinition;
  */
 public class DwdRequestParamsEncoder {
 
-    public DwdWfsRequestParams encode(WacodisJobDefinition jobDefinition) {
-    	
+	public DwdWfsRequestParams encode(String version, String typeName, WacodisJobDefinition jobDefinition,
+			Date startDate, Date endDate) {
 
-    	
-        return null;
-    }
+		DwdWfsRequestParams params = new DwdWfsRequestParams();
+
+		// Bounding Box
+		AbstractDataEnvelopeAreaOfInterest areaOfInterest = jobDefinition.getAreaOfInterest();
+		List<Float> coordinates = areaOfInterest.getExtent();
+		DirectPosition2D bottomLeft = new DirectPosition2D(coordinates.get(1), coordinates.get(0));
+		DirectPosition2D upperRight = new DirectPosition2D(coordinates.get(3), coordinates.get(2));
+		Envelope2D bounds = new Envelope2D(bottomLeft, upperRight);
+
+		params.setVersion(version);
+		params.setTypeName(typeName);
+		params.setBbox(bounds);
+		params.setStartDate(startDate);
+		params.setEndDate(endDate);
+
+		return params;
+	}
 
 }
