3ec5020762ec41e26b2e96e50c545698c0e6a070
Tim <tim.kurowski@hs-bochum.de>
2019-06-04 08:03:00 +0200

    finish DwdDataEnvelopeTest

--- dwd-observer/src/test/java/de/wacodis/dwd/cdc/DwdDataEnvelopeTest.java
+++ dwd-observer/src/test/java/de/wacodis/dwd/cdc/DwdDataEnvelopeTest.java
@@ -7,6 +7,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -15,14 +18,19 @@ import org.junit.jupiter.api.Test;
 import de.wacodis.api.model.DwdDataEnvelope;
 
 public class DwdDataEnvelopeTest {
+	
+	// dummy object
 	static DwdProductsMetadata metadata;
 	@BeforeAll
 	static void setup() throws ParseException {
 
+		// fill dummy object
 		metadata = new DwdProductsMetadata();
 		metadata.setServiceUrl("https://cdc.dwd.de:443/geoserver/CDC/wfs?");
 		metadata.setLayername("CDC:VGSL_FX_MN003");
 		metadata.setParameter("Tägliche Stationsmessungen der maximalen Windspitze in ca. 10 m Höhe in m/s");
+		
+		// bbox
 		List<Float> extent = new ArrayList<Float>();
 		extent.add(51.402f);
 		extent.add(6.966f);
@@ -30,9 +38,10 @@ public class DwdDataEnvelopeTest {
 		extent.add(6.969f);
 		metadata.setExtent(extent);
 
-		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss:SS'Z'");
-		Date startDate = df.parse("2019-04-24T01:00:00:00Z");
-		Date endDate = df.parse("2019-04-25T10:00:00:00Z");
+		// time frame
+		DateTimeFormatter df = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss:SS'Z'");
+		DateTime startDate = DateTime.parse("2019-04-24T01:00:00:00Z", df);
+		DateTime endDate = DateTime.parse("2019-04-25T10:00:00:00Z", df);
 		metadata.setStartDate(startDate);
 		metadata.setEndDate(endDate);
 
@@ -41,14 +50,16 @@ public class DwdDataEnvelopeTest {
 	@DisplayName("Test DwdDataEnvelope")
 	@Test
 	void testDataEnvelope() {
+		// test if DwdDataEnvelope has the same content
 		DwdDataEnvelope envelope = DwdProductsMetadataDecoder.decode(metadata);
 
+		// comparison
 		Assertions.assertEquals(envelope.getServiceUrl(), metadata.getServiceUrl());
 		Assertions.assertEquals(envelope.getLayerName(), metadata.getLayername());
 		Assertions.assertEquals(envelope.getParameter(), metadata.getParameter());
-		//Assertions.assertEquals(envelope.getAreaOfInterest().getExtent(), metadata.getExtent());
-		Assertions.assertEquals(envelope.getTimeFrame().getEndTime(), metadata.getStartDate());
-		//Assertions.assertEquals(envelope.getTimeFrame().getEndTime(), metadata.getEndDate());
+		Assertions.assertEquals(envelope.getAreaOfInterest().getExtent(), metadata.getExtent());
+		Assertions.assertEquals(envelope.getTimeFrame().getEndTime(), metadata.getEndDate());
+		Assertions.assertEquals(envelope.getTimeFrame().getEndTime(), metadata.getEndDate());
 
 	}
 }
