e00b2ecfe62c8ef9b0cb2587ff34e4df7eca71fc
Christian <christian.koert@hs-bochum.de>
2019-06-18 09:04:34 +0200

    TimeFrame and BBOX loops

--- dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdWfsRequestor.java
+++ dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdWfsRequestor.java
@@ -6,8 +6,10 @@
 package de.wacodis.dwd.cdc;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.geotools.data.DataStore;
@@ -25,8 +27,10 @@ import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.filter.And;
 import org.opengis.filter.FilterFactory2;
 import org.opengis.filter.PropertyIsBetween;
+import org.opengis.filter.expression.PropertyName;
 import org.opengis.filter.sort.SortBy;
 import org.opengis.filter.spatial.BBOX;
+import org.opengis.geometry.BoundingBox;
 
 /**
  * This class is responsible for requesting DWD FeatureServices for stationary
@@ -60,8 +64,8 @@ public class DwdWfsRequestor {
 		// Filter
 		FilterFactory2 ff = CommonFactoryFinder.getFilterFactory2(GeoTools.getDefaultHints());
 		BBOX bbox = ff.bbox(ff.property(geomName), params.getBbox());
-		PropertyIsBetween timeFilter = ff.between(ff.property("CDC:ZEITSTEMPEL"), ff.literal(params.getStartDate()),
-				ff.literal(params.getEndDate()));
+		PropertyIsBetween timeFilter = ff.between(
+				ff.property("CDC:ZEITSTEMPEL"), ff.literal(params.getStartDate()), ff.literal(params.getEndDate()));
 		And conjunction = ff.and(timeFilter, bbox);
 
 		// Query
@@ -76,67 +80,112 @@ public class DwdWfsRequestor {
 		// create DwdProductsMetaData
 		DwdProductsMetadata metadata = new DwdProductsMetadata();
 		//ReferencedEnvelope extent = source.getBounds(query);
-		ReferencedEnvelope extent = source.getFeatures(query).getBounds();
+		//ReferencedEnvelope extent = source.getFeatures(query).getBounds();
 		
 		// set parameters
 		// bbox
-		metadata.setExtent((float) extent.getMinX(), (float) extent.getMinY(), (float) extent.getMaxX(),
-				(float) extent.getMaxY());
+		
+		ArrayList<Float> extent = generateBBox(features);
+		metadata.setExtent(extent.get(0), extent.get(1),  extent.get(2),
+				extent.get(3));
+				
 		// name
 		metadata.setLayername(source.getInfo().getName());
 		// clearname
 		metadata.setParameter(source.getInfo().getTitle());
 		// timeframe
-		
+		ArrayList<DateTime> timeFrame = generateTimeFrame(features);
+		metadata.setStartDate(timeFrame.get(0));
+		metadata.setEndDate(timeFrame.get(1));
+		// serviceurl
+		metadata.setServiceUrl(url);
+		return metadata;
+	}
+
+	private static ArrayList<DateTime> generateTimeFrame(FeatureCollection<SimpleFeatureType, SimpleFeature> features) {
 		FeatureIterator<SimpleFeature> iterator = features.features();
 		DateTime startDate = new DateTime();
 		DateTime endDate= new DateTime();
+		ArrayList<DateTime> timeFrame = new ArrayList<DateTime>(2);
 		try {
-					
-		
 			for(int i=1;iterator.hasNext(); i++){
 				SimpleFeature feature = (SimpleFeature) iterator.next();
 				DateTime temp = (DateTime) feature.getAttribute("ZEITSTEMPEL");
-				
 				// Set start Values
 				if(i==1) {
 					startDate = temp;
 					endDate = temp;
-					metadata.setStartDate(startDate);
-					metadata.setEndDate(endDate);
+					timeFrame.add(startDate);
+					timeFrame.add(endDate);
 				}
 				
 				// Set StartDate or EndDate
 				if(temp.isBefore(startDate)) {
 					startDate = temp;
-					metadata.setStartDate(startDate);
+					timeFrame.add(startDate);
 				}	
 				
 				if(temp.isAfter(endDate)) {
 					endDate = temp;
-					metadata.setEndDate(endDate);
+					timeFrame.add(endDate);
 				}
 				
-				
-				
-				
-				
-				/*
-				
-				if(i ==1) {
-					metadata.setStartDate((DateTime) feature.getAttribute("ZEITSTEMPEL"));
-				}
-				if(!iterator.hasNext()) {
-					metadata.setEndDate((DateTime) feature.getAttribute("ZEITSTEMPEL"));
-				}
-				*/
 			}
 		} finally {
 			iterator.close();
 		}
-		// serviceurl
-		metadata.setServiceUrl(url);
-		return metadata;
+		return timeFrame;
+	}
+	
+	private static ArrayList<Float> generateBBox(FeatureCollection<SimpleFeatureType, SimpleFeature> features) {
+		FeatureIterator<SimpleFeature> iterator = features.features();
+		float xMin = Float.NaN;
+		float yMin = Float.NaN;
+		float xMax = Float.NaN;
+		float yMax = Float.NaN;
+		ArrayList<Float> extent = new ArrayList<Float>(4);
+		try {
+			for(int i=1;iterator.hasNext(); i++){
+				SimpleFeature feature = (SimpleFeature) iterator.next();
+				//float temp = feature.getAttribute("GEOM");
+				BoundingBox bBox = feature.getBounds();
+				// Set start Values
+				if(i==1) {
+					xMin = (float) bBox.getMinX();
+					yMin = (float) bBox.getMinY();
+					xMax = (float) bBox.getMaxX();
+					yMax = (float) bBox.getMaxY();
+					
+				}
+				
+				// Set actual Boundingbox
+				if(xMin < bBox.getMinX()) {
+					xMin = (float) bBox.getMinX();
+				}	
+				
+				if(yMin < bBox.getMinY()) {
+					yMin = (float) bBox.getMinY();
+			
+				}
+				if(xMax > bBox.getMaxX()) {
+					xMax = (float) bBox.getMaxX();
+				}	
+				
+				if(yMax < bBox.getMaxY()) {
+					yMax = (float) bBox.getMaxY();
+			
+				}
+				
+				extent.add(xMin);
+				extent.add(yMin);
+				extent.add(xMax);
+				extent.add(yMax);
+				
+			}
+		} finally {
+			iterator.close();
+		}
+		return extent;
 	}
 
 }
