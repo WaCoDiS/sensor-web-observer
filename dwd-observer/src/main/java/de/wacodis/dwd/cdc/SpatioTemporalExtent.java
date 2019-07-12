2ad67b841cf4e66e023bc961a1aff5e8e0efb73f
Christian <christian.koert@hs-bochum.de>
2019-07-12 11:04:18 +0200

    Creates Container Class for BBox and timestamp

---
+++ dwd-observer/src/main/java/de/wacodis/dwd/cdc/SpatioTemporalExtent.java
@@ -0,0 +1,27 @@
+package de.wacodis.dwd.cdc;
+
+import java.util.ArrayList;
+
+import org.joda.time.DateTime;
+
+public class SpatioTemporalExtent {
+	private ArrayList<DateTime> timeFrame;
+	private ArrayList<Float> bBox;
+	
+
+	public ArrayList<DateTime> getTimeFrame() {
+		return timeFrame;
+	}
+	public void setTimeFrame(ArrayList<DateTime> timeFrame) {
+		this.timeFrame = timeFrame;
+	}
+	public ArrayList<Float> getbBox() {
+		return bBox;
+	}
+	public void setbBox(ArrayList<Float> bBox) {
+		this.bBox = bBox;
+	}
+	
+	
+
+}
