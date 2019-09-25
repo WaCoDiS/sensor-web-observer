7b105bdf32af048fd975a4f2b39b52b9e52e940b
Tim <tim.kurowski@hs-bochum.de>
2019-09-25 07:14:26 +0200

    implement adding namespaces and bbox

--- dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdWfsRequestorBuilder.java
+++ dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdWfsRequestorBuilder.java
@@ -1,13 +1,17 @@
 package de.wacodis.dwd.cdc;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import org.apache.xmlbeans.XmlCursor;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 import org.opengis.filter.PropertyIsBetween;
 
+import net.opengis.fes.x20.BBOXDocument;
+import net.opengis.fes.x20.BBOXType;
 import net.opengis.fes.x20.BinaryLogicOpType;
 import net.opengis.fes.x20.ComparisonOpsType;
 import net.opengis.fes.x20.FilterDocument;
@@ -15,6 +19,7 @@ import net.opengis.fes.x20.FilterType;
 import net.opengis.fes.x20.FunctionType;
 import net.opengis.fes.x20.LogicOpsDocument;
 import net.opengis.fes.x20.LogicOpsType;
+import net.opengis.fes.x20.ValueReferenceDocument;
 import net.opengis.wfs.x20.GetFeatureDocument;
 import net.opengis.wfs.x20.GetFeatureType;
 import net.opengis.wfs.x20.PropertyNameDocument;
@@ -23,7 +28,7 @@ import net.opengis.wfs.x20.QueryDocument;
 import net.opengis.wfs.x20.QueryType;
 
 public class DwdWfsRequestorBuilder {
-	
+
 	// attributes
 	private static String xmlns = "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
 			+ "xmlns=\"http://www.opengis.net/wfs/2.0\" " + "xmlns:wfs=\"http://www.opengis.net/wfs/2.0\" "
@@ -37,7 +42,8 @@ public class DwdWfsRequestorBuilder {
 			+ "updateS-equence=\"727\" " + "xmlns:ogc=\"http://www.opengis.net/ogc\">";
 	public static final DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
 	// example GETurl:
-	//String propUrl;// https://cdc.dwd.de/geoserver/CDC/wfs?service=WFS&request=GetFeature
+	// String propUrl;//
+	// https://cdc.dwd.de/geoserver/CDC/wfs?service=WFS&request=GetFeature
 	String version;// &version=2.0.0
 	String typeName;// &typeName=CDC%3AVGSL_FX_MN003
 	List<String> bbox = new ArrayList<String>();// &bbox=51.200,6.700,51.500,7.300
@@ -49,7 +55,7 @@ public class DwdWfsRequestorBuilder {
 	public DwdWfsRequestorBuilder(DwdWfsRequestParams params) {
 		this.version = params.getVersion();
 		this.typeName = params.getTypeName();
-		for(int i=0; i<params.getBbox().size(); i++) {
+		for (int i = 0; i < params.getBbox().size(); i++) {
 			bbox.add(i, Float.toString(params.getBbox().get(i)));
 		}
 		this.startDate = params.getStartDate();
@@ -58,49 +64,78 @@ public class DwdWfsRequestorBuilder {
 	}
 
 	public String createXmlPostMessage() {
-		
-		
+
+		ArrayList<String[]> namespaces = new ArrayList<String[]>();
+		namespaces.add(new String[] { "xsi", "http://www.w3.org/2001/XMLSchema-instance" });
+		namespaces.add(new String[] { "", "http://www.opengis.net/wfs/2.0" });
+		namespaces.add(new String[] { "wfs", "http://www.opengis.net/wfs/2.0" });
+		namespaces.add(new String[] { "ows", "http://www.opengis.net/ows/1.1" });
+		namespaces.add(new String[] { "gml", "http://www.opengis.net/gml/3.2" });
+		namespaces.add(new String[] { "fes", "http://www.opengis.net/fes/2.0" });
+		namespaces.add(new String[] { "xlink", "http://www.w3.org/1999/xlink" });
+		namespaces.add(new String[] { "xs", "http://www.w3.org/2001/XMLSchema" });
+		namespaces.add(new String[] { "inspire_dls", "http://inspire.ec.europa.eu/schemas/inspire_dls/1.0" });
+		namespaces.add(new String[] { "inspire_common", "http://inspire.ec.europa.eu/schemas/common/1.0" });
+		namespaces.add(new String[] { "CDC", "https://cdc.dwd.de" });
+		namespaces.add(new String[] { "ogc", "http://www.opengis.net/ogc" });
+
+		ArrayList<String[]> attributes = new ArrayList<String[]>();
+		attributes.add(new String[] { "xsi", "schemaLocation",
+				"http://www.opengis.net/wfs/2.0 https://cdc.dwd.de:443/geoserver/schemas/wfs/2.0/wfs.xsd http://inspire.ec.europa.eu/schemas/inspire_dls/1.0 http://inspire.ec.europa.eu/schemas/inspire_dls/1.0/inspire_dls.xsd" });
+		// attributes.add(new String[] {"updateS-equence", "727"});
+
 		GetFeatureDocument getFeatureDoc = GetFeatureDocument.Factory.newInstance();
-        GetFeatureType getFeature = getFeatureDoc.addNewGetFeature();
-        getFeature.setService("WFS");
-        getFeature.setVersion(this.version);
-        getFeature.setOutputFormat(this.outputFormat);
-        
+		GetFeatureType getFeature = getFeatureDoc.addNewGetFeature();
+		getFeature.setService("WFS");
+		getFeature.setVersion(this.version);
+		getFeature.setOutputFormat(this.outputFormat);
 
-        QueryDocument queryDoc = QueryDocument.Factory.newInstance();
-        QueryType query = queryDoc.addNewQuery();
-        ArrayList<String> typeList = new ArrayList<String>();
-        typeList.add("layer_01");
-                
-        PropertyNameDocument propertyNameDoc = PropertyNameDocument.Factory.newInstance();
-        PropertyName propertyName = propertyNameDoc.addNewPropertyName();
-        propertyName.setStringValue("TestAttributeValue");
-        
-        FilterDocument filterDocument = FilterDocument.Factory.newInstance();
-        FilterType filter = filterDocument.addNewFilter();
-       
-        
-        LogicOpsDocument logicOpsDocument = LogicOpsDocument.Factory.newInstance();
-        //LogicOpsType andOperation = logicOpsDocument.addNewLogicOps(); 
-        BinaryLogicOpType andOperation = BinaryLogicOpType.Factory.newInstance();
-        filter.set(andOperation);
-        
-        query.set(propertyNameDoc);
-        query.setTypeNames(typeList);     
-        query.set(filterDocument);
-        getFeature.set(queryDoc);
+		// namespaces
+		XmlCursor cursor = getFeature.newCursor();
+		cursor.toNextToken();
+		for (int i = 0; i < namespaces.size(); i++) {
+			cursor.insertNamespace(namespaces.get(i)[0], namespaces.get(i)[1]);
+		}
+		cursor.insertAttributeWithValue(attributes.get(0)[1], attributes.get(0)[0], attributes.get(0)[2]);
 
-        System.out.println(getFeatureDoc.xmlText());
+		cursor.dispose();
 
-        
-        
-		
+		QueryDocument queryDoc = QueryDocument.Factory.newInstance();
+		QueryType query = queryDoc.addNewQuery();
+		ArrayList<String> typeList = new ArrayList<String>();
+		typeList.add("layer_01");
+
+		PropertyNameDocument propertyNameDoc = PropertyNameDocument.Factory.newInstance();
+		PropertyName propertyName = propertyNameDoc.addNewPropertyName();
+		propertyName.setStringValue("TestAttributeValue");
+
+		FilterDocument filterDocument = FilterDocument.Factory.newInstance();
+		FilterType filter = filterDocument.addNewFilter();
+		filter.addNewLogicOps();
+
+		BBOXDocument bboxDocument = BBOXDocument.Factory.newInstance();
+		BBOXType bbox2 = bboxDocument.addNewBBOX();
+
+		ValueReferenceDocument geomColumnDocument = ValueReferenceDocument.Factory.newInstance();
+		geomColumnDocument.setValueReference("CDC:GEOM");
+		bbox2.set(geomColumnDocument);
+
+		filter.set(bboxDocument);
+
+		query.set(propertyNameDoc);
+		query.setTypeNames(typeList);
+		query.set(filterDocument);
+		getFeature.set(queryDoc);
+
+		System.out.println(getFeatureDoc.xmlText());
+
 		StringBuffer wfsRequest = new StringBuffer();
 		// GetFeature-tag
-		wfsRequest.append("<wfs:GetFeature service=\"WFS\" version=\"" + version + "\" outputFormat=\"" + outputFormat + "\" ");
+		wfsRequest.append(
+				"<wfs:GetFeature service=\"WFS\" version=\"" + version + "\" outputFormat=\"" + outputFormat + "\" ");
 		wfsRequest.append(xmlns);
 		// Query-tag
-		wfsRequest.append("<wfs:Query typeNames=\""+ "CDC:VGSL_" + typeName + "\">");
+		wfsRequest.append("<wfs:Query typeNames=\"" + "CDC:VGSL_" + typeName + "\">");
 		wfsRequest.append("<fes:Filter><fes:And>"); // Filter
 		wfsRequest.append("<fes:BBOX>" + "                <fes:ValueReference>CDC:GEOM</fes:ValueReference>"
 				+ "                <gml:Envelope srsName=\"urn:ogc:def:crs:EPSG::4326\">"
@@ -113,7 +148,7 @@ public class DwdWfsRequestorBuilder {
 						"            	<fes:Literal>" + formatter.print(endDate) + "</fes:Literal>" + // 2019-06-02T10:00:00Z
 						"            </fes:PropertyIsBetween>"); // timeFrame
 		wfsRequest.append(" </fes:And></fes:Filter></wfs:Query>"); // end query
-		wfsRequest.append("</wfs:GetFeature>");	// end getFeature
+		wfsRequest.append("</wfs:GetFeature>"); // end getFeature
 
 		return wfsRequest.toString();
 	}
