f5b4ce9713ba2327395f8913714cdfbc3ef3f8ae
Christian <christian.koert@hs-bochum.de>
2019-10-11 08:38:32 +0200

    complete WFS Request

--- dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdWfsRequestorBuilder.java
+++ dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdWfsRequestorBuilder.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamReader;
 
 import org.apache.xmlbeans.XmlCursor;
@@ -14,6 +15,7 @@ import org.apache.xmlbeans.xml.stream.XMLInputStream;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
+import org.n52.oxf.xmlbeans.tools.XmlUtil;
 import org.opengis.filter.PropertyIsBetween;
 import org.opengis.filter.expression.Literal;
 import org.opengis.geometry.Envelope;
@@ -24,6 +26,7 @@ import net.opengis.fes.x20.AndDocument;
 import net.opengis.fes.x20.BBOXDocument;
 import net.opengis.fes.x20.BBOXType;
 import net.opengis.fes.x20.BinaryLogicOpType;
+import net.opengis.fes.x20.ComparisonOpsDocument;
 import net.opengis.fes.x20.ComparisonOpsType;
 import net.opengis.fes.x20.DWithinDocument;
 import net.opengis.fes.x20.FilterDocument;
@@ -36,6 +39,7 @@ import net.opengis.fes.x20.LogicOpsType;
 import net.opengis.fes.x20.LowerBoundaryType;
 import net.opengis.fes.x20.PropertyIsBetweenDocument;
 import net.opengis.fes.x20.PropertyIsBetweenType;
+import net.opengis.fes.x20.SortPropertyType;
 import net.opengis.fes.x20.SpatialOpsType;
 import net.opengis.fes.x20.UpperBoundaryType;
 import net.opengis.fes.x20.ValueReferenceDocument;
@@ -49,24 +53,19 @@ import net.opengis.wfs.x20.PropertyNameDocument;
 import net.opengis.wfs.x20.PropertyNameDocument.PropertyName;
 import net.opengis.wfs.x20.QueryDocument;
 import net.opengis.wfs.x20.QueryType;
+import net.opengis.wfs20.ValueReferenceType;
 
 public class DwdWfsRequestorBuilder {
 
-	// attributes
-	private static String xmlns = "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" "
-			+ "xmlns=\"http://www.opengis.net/wfs/2.0\" " + "xmlns:wfs=\"http://www.opengis.net/wfs/2.0\" "
-			+ "xmlns:ows=\"http://www.opengis.net/ows/1.1\" " + "xmlns:gml=\"http://www.opengis.net/gml/3.2\" "
-			+ "xmlns:fes=\"http://www.opengis.net/fes/2.0\" " + "xmlns:xlink=\"http://www.w3.org/1999/xlink\" "
-			+ "xmlns:xs=\"http://www.w3.org/2001/XMLSchema\" "
-			+ "xmlns:inspire_dls=\"http://inspire.ec.europa.eu/schemas/inspire_dls/1.0\" "
-			+ "xmlns:inspire_common=\"http://inspire.ec.europa.eu/schemas/common/1.0\" "
-			+ "xmlns:CDC=\"https://cdc.dwd.de\" "
-			+ "xsi:schemaLocation=\"http://www.opengis.net/wfs/2.0 https://cdc.dwd.de:443/geoserver/schemas/wfs/2.0/wfs.xsd http://inspire.ec.europa.eu/schemas/inspire_dls/1.0 http://inspire.ec.europa.eu/schemas/inspire_dls/1.0/inspire_dls.xsd\" "
-			+ "updateS-equence=\"727\" " + "xmlns:ogc=\"http://www.opengis.net/ogc\">";
+	// class attributes
 	public static final DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
-	// example GETurl:
-	// String propUrl;//
-	// https://cdc.dwd.de/geoserver/CDC/wfs?service=WFS&request=GetFeature
+	public static final String typeNamePrefix = "CDC:VGSL_";
+	public static final String geometryAttribute = "CDC:GEOM";
+	public static final String timestampAttribute = "CDC:ZEITSTEMPEL";
+	public static final String epsgUrn = "urn:ogc:def:crs:EPSG::4326";
+	public static final String serviceAttribute = "WFS";
+
+	// attributes
 	String version;// &version=2.0.0
 	String typeName;// &typeName=CDC%3AVGSL_FX_MN003
 	List<String> bbox = new ArrayList<String>();// &bbox=51.200,6.700,51.500,7.300
@@ -105,7 +104,6 @@ public class DwdWfsRequestorBuilder {
 		ArrayList<String[]> attributes = new ArrayList<String[]>();
 		attributes.add(new String[] { "xsi", "schemaLocation",
 				"http://www.opengis.net/wfs/2.0 https://cdc.dwd.de:443/geoserver/schemas/wfs/2.0/wfs.xsd http://inspire.ec.europa.eu/schemas/inspire_dls/1.0 http://inspire.ec.europa.eu/schemas/inspire_dls/1.0/inspire_dls.xsd" });
-		// attributes.add(new String[] {"updateS-equence", "727"});
 
 		// <GetFeature>
 		GetFeatureDocument getFeatureDoc = GetFeatureDocument.Factory.newInstance();
@@ -114,9 +112,9 @@ public class DwdWfsRequestorBuilder {
 		// <Query>
 		QueryDocument queryDoc = QueryDocument.Factory.newInstance();
 		QueryType query = queryDoc.addNewQuery();
-		
 		ArrayList<String> typeList = new ArrayList<String>();
-		typeList.add("CDC:VGSL_" + typeName);
+		typeList.add(typeNamePrefix + typeName);
+		
 		// <Filter>
 		FilterDocument filterDocument = FilterDocument.Factory.newInstance();
 		FilterType filter = filterDocument.addNewFilter();
@@ -124,63 +122,60 @@ public class DwdWfsRequestorBuilder {
 		// <And>
 		AndDocument andDocument = AndDocument.Factory.newInstance();
 		BinaryLogicOpType andType =  andDocument.addNewAnd();
-		
-
+	
 		// <BBOX>
 		BBOXDocument bboxDocument = BBOXDocument.Factory.newInstance();
 		BBOXType bboxType = bboxDocument.addNewBBOX();
 		
-		
 		// <ValueReference>
 		ValueReferenceDocument valueRefDocument = ValueReferenceDocument.Factory.newInstance();
-		valueRefDocument.setValueReference("CDC:GEOM");
+		valueRefDocument.setValueReference(geometryAttribute);
 		// </ValueReference>
-		bboxType.set(valueRefDocument);
+
 		
 		//<Envelope>
 		EnvelopeDocument envDoc = EnvelopeDocument.Factory.newInstance();
 		EnvelopeType envType = envDoc.addNewEnvelope();
-		envType.setSrsName("urn:ogc:def:crs:EPSG::4326");
+		envType.setSrsName(epsgUrn);
 		DirectPositionType lowerCorner = envType.addNewLowerCorner();
 		lowerCorner.setStringValue(bbox.get(0) + " " + bbox.get(1));
 		DirectPositionType upperCorner = envType.addNewUpperCorner();
 		upperCorner.setStringValue(bbox.get(2) + " " + bbox.get(3));
-
 		//</Envelope>
-		bboxType.set(envDoc);
-	
+		addXSAnyElement(bboxType, envType);
+		addXSAnyElement(bboxType, valueRefDocument.getExpression());
 		// </BBOX>
-		andType.set(bboxDocument);
-
+		addXSAnyElement(andType, bboxType);
+		
 		// <PropertyIsBetween>
 		PropertyIsBetweenDocument propBetweenDocument = PropertyIsBetweenDocument.Factory.newInstance();
 		PropertyIsBetweenType propBetweenType = propBetweenDocument.addNewPropertyIsBetween();
+		
 		// <ValueReference>
 		ValueReferenceDocument valueRefDocument2 = ValueReferenceDocument.Factory.newInstance();
-		valueRefDocument.setValueReference("CDC:ZEITSTEMPEL");
+		valueRefDocument2.setValueReference(timestampAttribute);
 		// </ValueReference>
-		propBetweenType.set(valueRefDocument2);
-		
+
 		// <Literal>
 		LiteralDocument litDoc = LiteralDocument.Factory.newInstance();
 		LiteralType litType1 = litDoc.addNewLiteral();
 		XmlString xmlString = XmlString.Factory.newValue(formatter.print(startDate));
 		litType1.set(xmlString);
-		propBetweenType.set(litDoc);
-		
+		//</Literal>
+
 		// <Literal>
 		LiteralDocument litDoc2 = LiteralDocument.Factory.newInstance();
 		LiteralType litType2 = litDoc2.addNewLiteral();
 		XmlString xmlString2 = XmlString.Factory.newValue(formatter.print(endDate));
 		litType2.set(xmlString2);
 		//</Literal>
-		propBetweenType.set(litDoc2);
 		
-		//</PropertyIsBetween>
-		andType.set(propBetweenDocument);
+		addXSAnyElement(propBetweenType, litType2);
+		addXSAnyElement(propBetweenType, litType1);
 		
-		
-
+		addXSAnyElement(propBetweenType, valueRefDocument2.getExpression());
+		// </PropertyIsBetween>
+		addXSAnyElement(andType, propBetweenType);
 
 		// </And>
 		filter.set(andDocument);
@@ -203,34 +198,23 @@ public class DwdWfsRequestorBuilder {
 
 		cursor.dispose();
 		
-		getFeature.setService("WFS");
+		// Set additional attributes
+		getFeature.setService(serviceAttribute);
 		getFeature.setVersion(this.version);
 		getFeature.setOutputFormat(this.outputFormat);
 
-		System.out.println(getFeatureDoc.xmlText());
-
-		StringBuffer wfsRequest = new StringBuffer();
-		// GetFeature-tag
-		wfsRequest.append(
-				"<wfs:GetFeature service=\"WFS\" version=\"" + version + "\" outputFormat=\"" + outputFormat + "\" ");
-		wfsRequest.append(xmlns);
-		// Query-tag
-		wfsRequest.append("<wfs:Query typeNames=\"" + "CDC:VGSL_" + typeName + "\">");
-		wfsRequest.append("<fes:Filter><fes:And>"); // Filter
-		wfsRequest.append("<fes:BBOX>" + "                <fes:ValueReference>CDC:GEOM</fes:ValueReference>"
-				+ "                <gml:Envelope srsName=\"urn:ogc:def:crs:EPSG::4326\">"
-				+ "                    <gml:lowerCorner>" + bbox.get(0) + " " + bbox.get(1) + "</gml:lowerCorner>"
-				+ "                    <gml:upperCorner>" + bbox.get(2) + " " + bbox.get(3) + "</gml:upperCorner>"
-				+ "                </gml:Envelope>" + "            </fes:BBOX>"); // BBOX
-		wfsRequest.append(
-				" <fes:PropertyIsBetween>" + "            	<fes:ValueReference>CDC:ZEITSTEMPEL</fes:ValueReference>"
-						+ "            	<fes:Literal>" + formatter.print(startDate) + "</fes:Literal>" + // 2019-06-02T06:00:00Z
-						"            	<fes:Literal>" + formatter.print(endDate) + "</fes:Literal>" + // 2019-06-02T10:00:00Z
-						"            </fes:PropertyIsBetween>"); // timeFrame
-		wfsRequest.append(" </fes:And></fes:Filter></wfs:Query>"); // end query
-		wfsRequest.append("</wfs:GetFeature>"); // end getFeature
-
-		return wfsRequest.toString();
+		return getFeatureDoc.xmlText();
 	}
+	
+	protected void addXSAnyElement(XmlObject target, XmlObject newChild) {
+		XmlCursor childCur = newChild.newCursor();
 
+		XmlCursor targetCur = target.newCursor();
+		targetCur.toNextToken();
+
+		childCur.moveXml(targetCur);
+
+		childCur.dispose();
+		targetCur.dispose();
+	}
 }
