9908e0661bc170ec6ed8626ed6a0c869998f8fad
Tim <tim.kurowski@hs-bochum.de>
2019-10-11 09:45:02 +0200

    refactor DwdWfsRequestorBuilder

--- dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdWfsRequestorBuilder.java
+++ dwd-observer/src/main/java/de/wacodis/dwd/cdc/DwdWfsRequestorBuilder.java
@@ -72,6 +72,8 @@ public class DwdWfsRequestorBuilder {
 	DateTime startDate;
 	DateTime endDate;
 	String outputFormat;// &outputformat=application%2Fjson
+	ArrayList<String[]> namespaces = new ArrayList<String[]>();
+	ArrayList<String[]> attributes = new ArrayList<String[]>();
 
 	// constructor
 	public DwdWfsRequestorBuilder(DwdWfsRequestParams params) {
@@ -85,9 +87,134 @@ public class DwdWfsRequestorBuilder {
 		this.outputFormat = params.getOutputFormat();
 	}
 
-	public String createXmlPostMessage(){
+	public String createXmlPostMessage() {
 
-		ArrayList<String[]> namespaces = new ArrayList<String[]>();
+		createXmlAttributesList();
+
+		// <GetFeature>
+		GetFeatureDocument getFeatureDoc = GetFeatureDocument.Factory.newInstance();
+		GetFeatureType getFeature = getFeatureDoc.addNewGetFeature();
+
+		// <Query>
+		QueryDocument queryDoc = QueryDocument.Factory.newInstance();
+		QueryType query = queryDoc.addNewQuery();
+		ArrayList<String> typeList = new ArrayList<String>();
+		typeList.add(typeNamePrefix + typeName);
+
+		// <Filter>
+		FilterDocument filterDocument = FilterDocument.Factory.newInstance();
+		FilterType filter = filterDocument.addNewFilter();
+
+		// <And>
+		AndDocument andDocument = AndDocument.Factory.newInstance();
+		BinaryLogicOpType andType = andDocument.addNewAnd();
+
+		// <BBOX>
+		BBOXType bboxType = createBboxElement();
+		// </BBOX>
+		addXSAnyElement(andType, bboxType);
+
+		// <PropertyIsBetween>
+		PropertyIsBetweenType propBetweenType = createPropertyIsBetweenElement();
+		// </PropertyIsBetween>
+		addXSAnyElement(andType, propBetweenType);
+
+		// </And>
+		filter.set(andDocument);
+		// </Filter>
+		query.set(filterDocument);
+		// </Query>
+		query.setTypeNames(typeList);
+		// </GetFeature>
+		getFeature.set(queryDoc);
+
+		// Attributes for <GetFeature>
+		// namespaces
+		addAttributesToElement(getFeature);
+		return getFeatureDoc.xmlText();
+	}
+
+	private void addAttributesToElement(GetFeatureType getFeature) {
+		XmlCursor cursor = getFeature.newCursor();
+		cursor.toNextToken();
+		for (int i = 0; i < namespaces.size(); i++) {
+			cursor.insertNamespace(namespaces.get(i)[0], namespaces.get(i)[1]);
+		}
+		for (int i = 0; i < attributes.size(); i++) {
+			cursor.insertAttributeWithValue(attributes.get(i)[1], attributes.get(i)[0], attributes.get(i)[2]);
+		}
+		cursor.dispose();
+	}
+
+	private PropertyIsBetweenType createPropertyIsBetweenElement() {
+		// <PropertyIsBetween>
+		PropertyIsBetweenDocument propBetweenDocument = PropertyIsBetweenDocument.Factory.newInstance();
+		PropertyIsBetweenType propBetweenType = propBetweenDocument.addNewPropertyIsBetween();
+
+		// <ValueReference>
+		ValueReferenceDocument valueRefDocument2 = ValueReferenceDocument.Factory.newInstance();
+		valueRefDocument2.setValueReference(timestampAttribute);
+		// </ValueReference>
+
+		// <Literal>
+		LiteralDocument litDoc = LiteralDocument.Factory.newInstance();
+		LiteralType litType1 = litDoc.addNewLiteral();
+		XmlString xmlString = XmlString.Factory.newValue(formatter.print(startDate));
+		litType1.set(xmlString);
+		// </Literal>
+
+		// <Literal>
+		LiteralDocument litDoc2 = LiteralDocument.Factory.newInstance();
+		LiteralType litType2 = litDoc2.addNewLiteral();
+		XmlString xmlString2 = XmlString.Factory.newValue(formatter.print(endDate));
+		litType2.set(xmlString2);
+		// </Literal>
+
+		addXSAnyElement(propBetweenType, litType2);
+		addXSAnyElement(propBetweenType, litType1);
+
+		addXSAnyElement(propBetweenType, valueRefDocument2.getExpression());
+		return propBetweenType;
+	}
+
+	private BBOXType createBboxElement() {
+		// <BBOX>
+		BBOXDocument bboxDocument = BBOXDocument.Factory.newInstance();
+		BBOXType bboxType = bboxDocument.addNewBBOX();
+
+		// <ValueReference>
+		ValueReferenceDocument valueRefDocument = ValueReferenceDocument.Factory.newInstance();
+		valueRefDocument.setValueReference(geometryAttribute);
+		// </ValueReference>
+
+		// <Envelope>
+		EnvelopeDocument envDoc = EnvelopeDocument.Factory.newInstance();
+		EnvelopeType envType = envDoc.addNewEnvelope();
+		envType.setSrsName(epsgUrn);
+		DirectPositionType lowerCorner = envType.addNewLowerCorner();
+		lowerCorner.setStringValue(bbox.get(0) + " " + bbox.get(1));
+		DirectPositionType upperCorner = envType.addNewUpperCorner();
+		upperCorner.setStringValue(bbox.get(2) + " " + bbox.get(3));
+		// </Envelope>
+		addXSAnyElement(bboxType, envType);
+		addXSAnyElement(bboxType, valueRefDocument.getExpression());
+		return bboxType;
+	}
+
+	protected void addXSAnyElement(XmlObject target, XmlObject newChild) {
+		XmlCursor childCur = newChild.newCursor();
+
+		XmlCursor targetCur = target.newCursor();
+		targetCur.toNextToken();
+
+		childCur.moveXml(targetCur);
+
+		childCur.dispose();
+		targetCur.dispose();
+	}
+
+	private void createXmlAttributesList() {
+		// namespaces
 		namespaces.add(new String[] { "xsi", "http://www.w3.org/2001/XMLSchema-instance" });
 		namespaces.add(new String[] { "", "http://www.opengis.net/wfs/2.0" });
 		namespaces.add(new String[] { "wfs", "http://www.opengis.net/wfs/2.0" });
@@ -100,121 +227,11 @@ public class DwdWfsRequestorBuilder {
 		namespaces.add(new String[] { "inspire_common", "http://inspire.ec.europa.eu/schemas/common/1.0" });
 		namespaces.add(new String[] { "CDC", "https://cdc.dwd.de" });
 		namespaces.add(new String[] { "ogc", "http://www.opengis.net/ogc" });
-
-		ArrayList<String[]> attributes = new ArrayList<String[]>();
+		// attributes
 		attributes.add(new String[] { "xsi", "schemaLocation",
 				"http://www.opengis.net/wfs/2.0 https://cdc.dwd.de:443/geoserver/schemas/wfs/2.0/wfs.xsd http://inspire.ec.europa.eu/schemas/inspire_dls/1.0 http://inspire.ec.europa.eu/schemas/inspire_dls/1.0/inspire_dls.xsd" });
-
-		// <GetFeature>
-		GetFeatureDocument getFeatureDoc = GetFeatureDocument.Factory.newInstance();
-		GetFeatureType getFeature = getFeatureDoc.addNewGetFeature();
-		
-		// <Query>
-		QueryDocument queryDoc = QueryDocument.Factory.newInstance();
-		QueryType query = queryDoc.addNewQuery();
-		ArrayList<String> typeList = new ArrayList<String>();
-		typeList.add(typeNamePrefix + typeName);
-		
-		// <Filter>
-		FilterDocument filterDocument = FilterDocument.Factory.newInstance();
-		FilterType filter = filterDocument.addNewFilter();
-		
-		// <And>
-		AndDocument andDocument = AndDocument.Factory.newInstance();
-		BinaryLogicOpType andType =  andDocument.addNewAnd();
-	
-		// <BBOX>
-		BBOXDocument bboxDocument = BBOXDocument.Factory.newInstance();
-		BBOXType bboxType = bboxDocument.addNewBBOX();
-		
-		// <ValueReference>
-		ValueReferenceDocument valueRefDocument = ValueReferenceDocument.Factory.newInstance();
-		valueRefDocument.setValueReference(geometryAttribute);
-		// </ValueReference>
-
-		
-		//<Envelope>
-		EnvelopeDocument envDoc = EnvelopeDocument.Factory.newInstance();
-		EnvelopeType envType = envDoc.addNewEnvelope();
-		envType.setSrsName(epsgUrn);
-		DirectPositionType lowerCorner = envType.addNewLowerCorner();
-		lowerCorner.setStringValue(bbox.get(0) + " " + bbox.get(1));
-		DirectPositionType upperCorner = envType.addNewUpperCorner();
-		upperCorner.setStringValue(bbox.get(2) + " " + bbox.get(3));
-		//</Envelope>
-		addXSAnyElement(bboxType, envType);
-		addXSAnyElement(bboxType, valueRefDocument.getExpression());
-		// </BBOX>
-		addXSAnyElement(andType, bboxType);
-		
-		// <PropertyIsBetween>
-		PropertyIsBetweenDocument propBetweenDocument = PropertyIsBetweenDocument.Factory.newInstance();
-		PropertyIsBetweenType propBetweenType = propBetweenDocument.addNewPropertyIsBetween();
-		
-		// <ValueReference>
-		ValueReferenceDocument valueRefDocument2 = ValueReferenceDocument.Factory.newInstance();
-		valueRefDocument2.setValueReference(timestampAttribute);
-		// </ValueReference>
-
-		// <Literal>
-		LiteralDocument litDoc = LiteralDocument.Factory.newInstance();
-		LiteralType litType1 = litDoc.addNewLiteral();
-		XmlString xmlString = XmlString.Factory.newValue(formatter.print(startDate));
-		litType1.set(xmlString);
-		//</Literal>
-
-		// <Literal>
-		LiteralDocument litDoc2 = LiteralDocument.Factory.newInstance();
-		LiteralType litType2 = litDoc2.addNewLiteral();
-		XmlString xmlString2 = XmlString.Factory.newValue(formatter.print(endDate));
-		litType2.set(xmlString2);
-		//</Literal>
-		
-		addXSAnyElement(propBetweenType, litType2);
-		addXSAnyElement(propBetweenType, litType1);
-		
-		addXSAnyElement(propBetweenType, valueRefDocument2.getExpression());
-		// </PropertyIsBetween>
-		addXSAnyElement(andType, propBetweenType);
-
-		// </And>
-		filter.set(andDocument);
-		// </Filter>
-		query.set(filterDocument);
-		// </Query>
-		query.setTypeNames(typeList);
-		// </GetFeature>
-		getFeature.set(queryDoc);
-		
-		
-		// Attributes for <GetFeature>
-		// namespaces
-		XmlCursor cursor = getFeature.newCursor();
-		cursor.toNextToken();
-		for (int i = 0; i < namespaces.size(); i++) {
-			cursor.insertNamespace(namespaces.get(i)[0], namespaces.get(i)[1]);
-		}
-		cursor.insertAttributeWithValue(attributes.get(0)[1], attributes.get(0)[0], attributes.get(0)[2]);
-
-		cursor.dispose();
-		
-		// Set additional attributes
-		getFeature.setService(serviceAttribute);
-		getFeature.setVersion(this.version);
-		getFeature.setOutputFormat(this.outputFormat);
-
-		return getFeatureDoc.xmlText();
-	}
-	
-	protected void addXSAnyElement(XmlObject target, XmlObject newChild) {
-		XmlCursor childCur = newChild.newCursor();
-
-		XmlCursor targetCur = target.newCursor();
-		targetCur.toNextToken();
-
-		childCur.moveXml(targetCur);
-
-		childCur.dispose();
-		targetCur.dispose();
+		attributes.add(new String[] { "", "service", serviceAttribute });
+		attributes.add(new String[] { "", "version", this.version });
+		attributes.add(new String[] { "", "outputFormat", this.outputFormat });
 	}
 }
