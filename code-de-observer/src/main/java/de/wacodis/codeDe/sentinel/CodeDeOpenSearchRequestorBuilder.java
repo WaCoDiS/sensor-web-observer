ff898c8f9f7a4d7fc9fbeb8308dbcf8f377aeabe
Tim <tim.kurowski@hs-bochum.de>
2019-12-02 08:55:19 +0100

    implement requestorBuilderTest

--- code-de-observer/src/main/java/de/wacodis/codeDe/sentinel/CodeDeOpenSearchRequestorBuilder.java
+++ code-de-observer/src/main/java/de/wacodis/codeDe/sentinel/CodeDeOpenSearchRequestorBuilder.java
@@ -1,29 +1,32 @@
 package de.wacodis.codeDe.sentinel;
 
 import de.wacodis.codeDe.CodeDeJob;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 public class CodeDeOpenSearchRequestorBuilder {
 
     public static final String SERVICE_URL = "https://catalog.code-de.org/opensearch/request/?";
     public static final String HTTP_ACCEPT = "httpAccept=application/atom%2Bxml";
     public static final String PARENT_IDENTIFIER_PREFIX = "EOP:CODE-DE:";
+    public static final DateTimeFormatter FORMATTER = DateTimeFormat.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
     public String buildGetRequestUrl(CodeDeRequestParams params){
         // parentIdentifier
         String parentIdentifier = CodeDeJob.PARENT_IDENTIFIER_KEY + "=" + PARENT_IDENTIFIER_PREFIX + params.getParentIdentifier();
         // dates
-        String startDate = CodeDeJob.START_DATE_KEY + "=" + params.getStartDate().toString();
-        String endDate = CodeDeJob.END_DATE_KEY + "=" + params.getEndDate().toString();
+        String startDate = CodeDeJob.START_DATE_KEY + "=" + params.getStartDate().toString(FORMATTER);
+        String endDate = CodeDeJob.END_DATE_KEY + "=" + params.getEndDate().toString(FORMATTER);
         // bbox
         String bboxContent = params.getBbox().toString();
-        bboxContent.replace("[", "");
-        bboxContent.replace("]", "");
-        bboxContent.replace(" ", "");
+        bboxContent = bboxContent.replace("[", "");
+        bboxContent =  bboxContent.replace("]", "");
+        bboxContent = bboxContent.replace(" ", "");
         String bbox = CodeDeJob.BBOX_KEY + "=" + bboxContent;
         // cloud coverage
         String cloudCoverContent = params.getCloudCover().toString();
-        cloudCoverContent.replace(" ", "");
-        String cloudCover = CodeDeJob.CLOUD_COVER_KEY + cloudCoverContent;
+        cloudCoverContent = cloudCoverContent.replace(" ", "");
+        String cloudCover = CodeDeJob.CLOUD_COVER_KEY + "=" + cloudCoverContent;
         // put together
         String getRequestUrl = SERVICE_URL
                 + HTTP_ACCEPT + "&"
--- code-de-observer/src/test/java/de/wacodis/codeDe/sentinel/CodeDeOpenSearchRequestorBuilderTest.java
+++ code-de-observer/src/test/java/de/wacodis/codeDe/sentinel/CodeDeOpenSearchRequestorBuilderTest.java
@@ -1,7 +1,48 @@
 package de.wacodis.codeDe.sentinel;
 
+import org.joda.time.DateTime;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CodeDeOpenSearchRequestorBuilderTest {
+
+    private static CodeDeRequestParams params;
+
+    @BeforeAll
+    static void setup(){
+        String parentIdentifier = "S2_MSI_L2A";
+        DateTime startDate = DateTime.parse("2019-10-01T00:00:00Z", CodeDeOpenSearchRequestorBuilder.FORMATTER);
+        DateTime endDate = DateTime.parse("2019-10-31T00:00:00Z", CodeDeOpenSearchRequestorBuilder.FORMATTER);
+        List<Float> bbox = new ArrayList<Float>();
+        bbox.add(6.96f);
+        bbox.add(50.9f);
+        bbox.add(8.02f);
+        bbox.add(51.2f);
+        List<Byte> cloudCover = new ArrayList<Byte>();
+        cloudCover.add((byte) 0);
+        cloudCover.add((byte) 40);
+        params = new CodeDeRequestParams(parentIdentifier, startDate, endDate, bbox, cloudCover);
+    }
+
+    @Test
+    void testBuilder(){
+        CodeDeOpenSearchRequestorBuilder requestorBuilder = new CodeDeOpenSearchRequestorBuilder();
+        String actualRequestUrl = requestorBuilder.buildGetRequestUrl(params);
+        String expectedRequestUrl = "https://catalog.code-de.org/opensearch/request/?" +
+                "httpAccept=application/atom%2Bxml&" +
+                "parentIdentifier=EOP:CODE-DE:S2_MSI_L2A&" +
+                "startDate=2019-10-01T00:00:00Z&" +
+                "endDate=2019-10-31T00:00:00Z&" +
+                "bbox=6.96,50.9,8.02,51.2&" +
+                "cloudCover=[0,40]";
+        Assertions.assertEquals(expectedRequestUrl, actualRequestUrl);
+    }
+
 
 }
