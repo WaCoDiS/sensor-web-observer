14b4c406b3813cb84d516b0b4c6061fe808e2220
Tim <tim.kurowski@hs-bochum.de>
2020-01-13 08:30:28 +0100

    test only the first element

--- code-de-observer/src/test/java/de/wacodis/codeDe/sentinel/CodeDeResponseResolverTest.java
+++ code-de-observer/src/test/java/de/wacodis/codeDe/sentinel/CodeDeResponseResolverTest.java
@@ -22,8 +22,7 @@ import java.util.List;
 import java.util.Map;
 
 class CodeDeResponseResolverTest {
-    
-    private static ArrayList<String> expectedDownloadLinks;
+
     private static CodeDeResponseResolver resolver;
     private static Document xmlDoc;
     private static DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
@@ -53,58 +52,43 @@ class CodeDeResponseResolverTest {
     void testGetDownloadLink() throws IOException, SAXException, XPathExpressionException {
 
         // expected download links
-        expectedDownloadLinks = new ArrayList<>();
-        expectedDownloadLinks.add("https://code-de.org/download/S2B_MSIL2A_20191012T103029_N0213_R108_T32ULB_20191012T135838.SAFE.zip");
-        expectedDownloadLinks.add("https://code-de.org/download/S2B_MSIL2A_20191012T103029_N0213_R108_T32UMB_20191012T135838.SAFE.zip");
-        expectedDownloadLinks.add("https://code-de.org/download/S2B_MSIL2A_20191012T103029_N0213_R108_T31UGS_20191012T135838.SAFE.zip");
+        String expectedDownloadLink = "https://code-de.org/download/S2B_MSIL2A_20191012T103029_N0213_R108_T32ULB_20191012T135838.SAFE.zip";
 
-        // actual downloadLinks
+        // actual download link
         InputStream openSearchResponseStream = this.getClass().getResourceAsStream("/catalog.code-de.org.xml");
         xmlDoc = db.parse(openSearchResponseStream);
-        String xPathString="/a:feed/a:entry";
+        String xPathString="/a:feed/a:entry[1]";
         XPathExpression expression = xpath.compile(xPathString);
-        NodeList nodeList = (NodeList) expression.evaluate(xmlDoc, XPathConstants.NODESET);
+        Node node = (Node) expression.evaluate(xmlDoc, XPathConstants.NODE);
 
-        List<String> actualDownloadLinks = new ArrayList<>();
-        for(int i = 0; i < nodeList.getLength(); i++){
-            Node node = nodeList.item(i);
             Document newDocument = db.newDocument();
             Node importedNode = newDocument.importNode(node, true);
             newDocument.appendChild(importedNode);
-            String downloadLink = resolver.getDownloadLink(newDocument);
-            actualDownloadLinks.add(downloadLink);
-        }
-        Assertions.assertEquals(expectedDownloadLinks, actualDownloadLinks);
+            String actualDownloadLink = resolver.getDownloadLink(newDocument);
+
+        Assertions.assertEquals(expectedDownloadLink, actualDownloadLink);
     }
 
     @Test
     void testGetMetadataLink() throws IOException, SAXException, XPathExpressionException {
 
-        // expected download links
-        expectedDownloadLinks = new ArrayList<>();
-        expectedDownloadLinks.add("https://catalog.code-de.org/opensearch/request/?httpAccept=application/gml%2Bxml&amp;parentIdentifier=EOP:CODE-DE:S2_MSI_L2A&amp;uid=EOP:CODE-DE:S2_MSI_L2A:/S2B_MSIL2A_20191012T103029_N0213_R108_T32ULB_20191012T135838&amp;recordSchema=om");
-        expectedDownloadLinks.add("https://catalog.code-de.org/opensearch/request/?httpAccept=application/gml%2Bxml&amp;parentIdentifier=EOP:CODE-DE:S2_MSI_L2A&amp;uid=EOP:CODE-DE:S2_MSI_L2A:/S2B_MSIL2A_20191012T103029_N0213_R108_T32UMB_20191012T135838&amp;recordSchema=om");
-        expectedDownloadLinks.add("https://catalog.code-de.org/opensearch/request/?httpAccept=application/gml%2Bxml&amp;parentIdentifier=EOP:CODE-DE:S2_MSI_L2A&amp;uid=EOP:CODE-DE:S2_MSI_L2A:/S2B_MSIL2A_20191012T103029_N0213_R108_T31UGS_20191012T135838&amp;recordSchema=om");
+        // expected metadata link
+        String expectedMetadataLink = "https://catalog.code-de.org/opensearch/request/?httpAccept=application/gml%2Bxml&amp;parentIdentifier=EOP:CODE-DE:S2_MSI_L2A&amp;uid=EOP:CODE-DE:S2_MSI_L2A:/S2B_MSIL2A_20191012T103029_N0213_R108_T32ULB_20191012T135838&amp;recordSchema=om";
 
-        // actual metadataLinks
+        // actual metadataLink
         InputStream openSearchResponseStream = this.getClass().getResourceAsStream("/catalog.code-de.org.xml");
         xmlDoc = db.parse(openSearchResponseStream);
-        String xPathString="/a:feed/a:entry";
+        String xPathString="/a:feed/a:entry[1]";
         XPathExpression expression = xpath.compile(xPathString);
-        NodeList nodeList = (NodeList) expression.evaluate(xmlDoc, XPathConstants.NODESET);
+        Node node = (Node) expression.evaluate(xmlDoc, XPathConstants.NODE);
 
-        List<String> actualMetadataLinks = new ArrayList<>();
-        for(int i = 0; i < nodeList.getLength(); i++){
-            Node node = nodeList.item(i);
             Document newDocument = db.newDocument();
             Node importedNode = newDocument.importNode(node, true);
             newDocument.appendChild(importedNode);
             String metadataLink = resolver.getMetaDataLink(newDocument);
-            String metadataLinkModified = metadataLink.replaceAll("&", "&amp;");
-            actualMetadataLinks.add(metadataLinkModified);
+            String actualMetadataLink = metadataLink.replaceAll("&", "&amp;");
 
-        }
-        Assertions.assertEquals(expectedDownloadLinks, actualMetadataLinks);
+        Assertions.assertEquals(expectedMetadataLink, actualMetadataLink);
 
     }
 
@@ -137,56 +121,35 @@ class CodeDeResponseResolverTest {
     @Test
     void testGetTimeFrame() throws IOException, SAXException, XPathExpressionException {
 
-        // expected cloud coverage
-        List<List<DateTime>> expectedTimeFrames = new ArrayList<>();
-        List<DateTime> firstPicture = new ArrayList<DateTime>(){
+        // expected time frame
+        List<DateTime> expectedTimeFrame = new ArrayList<DateTime>(){
             {
                 add(DateTime.parse("2019-10-12T10:30:29.024Z", CodeDeResponseResolver.FORMATTER));
                 add(DateTime.parse("2019-10-12T10:30:29.024Z", CodeDeResponseResolver.FORMATTER));
             }
         };
-        List<DateTime> secondPicture = new ArrayList<DateTime>(){
-            {
-                add(DateTime.parse("2019-10-12T10:30:29.024Z", CodeDeResponseResolver.FORMATTER));
-                add(DateTime.parse("2019-10-12T10:30:29.024Z", CodeDeResponseResolver.FORMATTER));
-            }
-        };
-        List<DateTime> thirdPicture = new ArrayList<DateTime>(){
-            {
-                add(DateTime.parse("2019-10-12T10:30:29.024Z", CodeDeResponseResolver.FORMATTER));
-                add(DateTime.parse("2019-10-12T10:30:29.024Z", CodeDeResponseResolver.FORMATTER));
-            }
-        };
-        expectedTimeFrames.add(firstPicture);
-        expectedTimeFrames.add(secondPicture);
-        expectedTimeFrames.add(thirdPicture);
 
-        // actual cloud coverage
+        // actual time frame
         InputStream document = this.getClass().getResourceAsStream("/catalog.code-de.org.xml");
         xmlDoc = db.parse(document);
         String xPathString="/a:feed/a:entry";
         XPathExpression expression = xpath.compile(xPathString);
         NodeList nodeList = (NodeList) expression.evaluate(xmlDoc, XPathConstants.NODESET);
 
-        List<List<DateTime>> actualTimeFrames = new ArrayList<>();
-        for(int i = 0; i < nodeList.getLength(); i++){
-            Node node = nodeList.item(i);
+            Node node = nodeList.item(0);
             Document newDocument = db.newDocument();
             Node importedNode = newDocument.importNode(node, true);
             newDocument.appendChild(importedNode);
-            List<DateTime> timeFrame = resolver.getTimeFrame(newDocument);
-            actualTimeFrames.add(timeFrame);
-        }
+            List<DateTime> actualTimeFrame = resolver.getTimeFrame(newDocument);
 
-        Assertions.assertEquals(expectedTimeFrames, actualTimeFrames);
+        Assertions.assertEquals(expectedTimeFrame, actualTimeFrame);
     }
 
     @Test
     void testGetBbox() throws IOException, SAXException, XPathExpressionException {
 
-        // expected cloud coverage
-        List<List<Float>> expectedbbox = new ArrayList<>();
-        List<Float> firstPicture = new ArrayList<Float>(){
+        // expected bbox
+        List<Float> expectedbbox = new ArrayList<Float>(){
             {
                 add(50.4368368428495f);
                 add(6.589443020581445f);
@@ -194,42 +157,19 @@ class CodeDeResponseResolverTest {
                 add(7.729300891794365f);
             }
         };
-        List<Float> secondPicture = new ArrayList<Float>(){
-            {
-                add(50.455265339610506f);
-                add(7.560547850091258f);
-                add(51.451098078258774f);
-                add(9.140457957691327f);
-            }
-        };
-        List<Float> thirdPicture = new ArrayList<Float>(){
-            {
-                add(50.38212225093181f);
-                add(6.577303273387585f);
-                add(51.38158890366703f);
-                add(7.450589699013681f);
-            }
-        };
-        expectedbbox.add(firstPicture);
-        expectedbbox.add(secondPicture);
-        expectedbbox.add(thirdPicture);
 
-        // actual cloud coverage
-        InputStream bboxDoc1 = this.getClass().getResourceAsStream("/catalog.code-de.org.xml");
-        xmlDoc = db.parse(bboxDoc1);
+        // actual bbox
+        InputStream bboxDoc = this.getClass().getResourceAsStream("/catalog.code-de.org.xml");
+        xmlDoc = db.parse(bboxDoc);
         String xPathString="/a:feed/a:entry";
         XPathExpression expression = xpath.compile(xPathString);
         NodeList nodeList = (NodeList) expression.evaluate(xmlDoc, XPathConstants.NODESET);
 
-        List<List<Float>> actualBbox = new ArrayList<>();
-        for(int i = 0; i < nodeList.getLength(); i++){
-            Node node = nodeList.item(i);
+            Node node = nodeList.item(0);
             Document newDocument = db.newDocument();
             Node importedNode = newDocument.importNode(node, true);
             newDocument.appendChild(importedNode);
-            List<Float> bbox = resolver.getBbox(newDocument);
-            actualBbox.add(bbox);
-        }
+            List<Float> actualBbox = resolver.getBbox(newDocument);
 
         Assertions.assertEquals(expectedbbox, actualBbox);
     }
