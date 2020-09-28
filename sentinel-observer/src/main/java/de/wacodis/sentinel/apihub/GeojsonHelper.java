abe338db1f1dfb01cdfe586dd18b93fafe927dd0
Sebastian Drost <s.drost@52north.org>
2020-09-28 15:40:55 +0200

    Adjust GeoJSON geometry creation to match geographic coordinate order

--- sentinel-observer/src/main/java/de/wacodis/sentinel/apihub/GeojsonHelper.java
+++ sentinel-observer/src/main/java/de/wacodis/sentinel/apihub/GeojsonHelper.java
@@ -1,8 +1,7 @@
 package de.wacodis.sentinel.apihub;
 
-import org.locationtech.jts.geom.Geometry;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.PrecisionModel;
+import org.locationtech.jts.geom.*;
+import org.locationtech.jts.geom.util.GeometryEditor;
 import org.locationtech.jts.io.geojson.GeoJsonWriter;
 import org.locationtech.jts.io.gml2.GMLReader;
 import org.slf4j.Logger;
@@ -13,8 +12,11 @@ import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.util.Arrays;
 
 /**
+ * Helper class for dealing with GeoJSON
+ *
  * @author <a href="mailto:s.drost@52north.org">Sebastian Drost</a>
  */
 @Component
@@ -28,16 +30,34 @@ public class GeojsonHelper implements InitializingBean {
 
     @Override
     public void afterPropertiesSet() throws Exception {
-        GeometryFactory factory = new GeometryFactory(new PrecisionModel(PrecisionModel.FLOATING), 4326);
+        factory = new GeometryFactory(new PrecisionModel(PrecisionModel.FLOATING), 4326);
         gmlReader = new GMLReader();
         geojsonWriter = new GeoJsonWriter();
+        geojsonWriter.setEncodeCRS(false);
     }
 
+    /**
+     * Decodes a GML geometry string to GeoJSON
+     *
+     * @param gmlGeom GML geometry string
+     * @return GeoJSON string
+     */
     public String decodeGml(String gmlGeom) {
         String geojsonFootprint = null;
         try {
             Geometry geom = gmlReader.read(gmlGeom, factory);
-            geojsonFootprint = geojsonWriter.write(geom);
+            // As JTS does not know anything about CRS
+            // the coordinate order has to be changed
+            // to be compliant with GeoJSON
+            GeometryEditor editor = new GeometryEditor();
+            Geometry editedGeom = editor.edit(geom, new GeometryEditor.CoordinateOperation() {
+                @Override
+                public Coordinate[] edit(Coordinate[] coordinates, Geometry geometry) {
+                    Coordinate[] switchedXY = new Coordinate[coordinates.length];
+                    return Arrays.stream(coordinates).map(c -> new Coordinate(c.getY(), c.getX())).toArray(Coordinate[]::new);
+                }
+            });
+            geojsonFootprint = geojsonWriter.write(editedGeom);
         } catch (SAXException | IOException | ParserConfigurationException ex) {
             LOG.warn("Could not decode GML footprint. Cause: {}", ex.getMessage());
             LOG.debug("Could not decode GML footprint.", ex);
