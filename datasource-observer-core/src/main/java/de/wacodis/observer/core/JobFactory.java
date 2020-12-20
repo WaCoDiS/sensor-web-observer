8b7256f0c64f8fac9b80c15c08339ccaab86a249
Christian Danowski-Buhren <christian.danowski-buhren@hs-bochum.de>
2020-12-20 21:55:47 +0100

    implement generic factory level based configuration of temporalCoverage observation period for job execution

--- datasource-observer-core/src/main/java/de/wacodis/observer/core/JobFactory.java
+++ datasource-observer-core/src/main/java/de/wacodis/observer/core/JobFactory.java
@@ -1,18 +1,28 @@
 package de.wacodis.observer.core;
 
-import de.wacodis.observer.model.AbstractSubsetDefinition;
-import de.wacodis.observer.model.WacodisJobDefinition;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.joda.time.DateTime;
+import org.joda.time.Period;
+import org.joda.time.format.ISOPeriodFormat;
 import org.quartz.JobBuilder;
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import de.wacodis.observer.model.AbstractSubsetDefinition;
+import de.wacodis.observer.model.AbstractSubsetDefinitionTemporalCoverage;
+import de.wacodis.observer.model.AbstractWacodisJobExecutionEvent;
+import de.wacodis.observer.model.AbstractWacodisJobExecutionEvent.EventTypeEnum;
+import de.wacodis.observer.model.SingleJobExecutionEvent;
+import de.wacodis.observer.model.WacodisJobDefinition;
+import de.wacodis.observer.model.WacodisJobDefinitionExecution;
+import de.wacodis.observer.model.WacodisJobDefinitionTemporalCoverage;
 
 /**
  * Factory-Pattern Interface for SubsetDefinitionJobs
@@ -97,6 +107,7 @@ public interface JobFactory {
             jobId = String.join("_", jobId, extent);
 
             LOG.debug("Initialize a potential quartz job with jobId '{}' and groupName '{}' for input subsetDefinition \n{}", jobId, jobGroupName, subsetDefinition);
+            data_cloned = configureFirstDataQueryPeriod(job, data_cloned, subsetDefinition);
             JobBuilder jobBuilder = initializeJobBuilder(job, data_cloned, subsetDefinition);
             // store durably is required in order to replace an already existing job in scheduler to update its jobDataMap
             JobDetail quartzJob = jobBuilder.withIdentity(jobId, jobGroupName).storeDurably(true).build();
@@ -106,7 +117,143 @@ public interface JobFactory {
         return quartzJobs;
     }
 
-    /**
+    default JobDataMap configureFirstDataQueryPeriod(WacodisJobDefinition job, JobDataMap data_cloned,
+			AbstractSubsetDefinition subsetDefinition) {
+    	
+    	/*
+    	 * Analyse subsetDefinition and job information to detect and set the query start and end date for the very first data query
+    	 * 
+    	 * it depends on several aspects such as job type, execution time or job/input specific settings.
+    	 * 
+    	 * basic idea: determine execution date of job --> determine "backwards" observer settings according to specific WacodisJob type and settings
+    	 * --> then consider observing backwards period from execution date and set it as startDate and endDate for first execution
+    	 * 
+    	 * later executions will consider endDate from previous query
+    	 */
+    	
+    	WacodisJobDefinitionExecution execution = job.getExecution();
+        AbstractWacodisJobExecutionEvent event = execution.getEvent();
+        if (event != null && event.getEventType().equals(EventTypeEnum.SINGLEJOBEXECUTIONEVENT)) {
+            // specification of a job, that shall only be executed once
+            // e.g. for on demand jobs
+        	
+
+            data_cloned = this.configureFirstDataQueryPeriod_singleExecutionJob(job, data_cloned,
+        			subsetDefinition, (SingleJobExecutionEvent) event);
+        } else {
+        	data_cloned = this.configureFirstDataQueryPeriod_regularExecutionJob(job, data_cloned,
+        			subsetDefinition);
+        }
+    	
+    	
+    	return data_cloned;
+    };
+
+	default JobDataMap configureFirstDataQueryPeriod_regularExecutionJob(WacodisJobDefinition job, JobDataMap data_cloned,
+			AbstractSubsetDefinition subsetDefinition) {
+		
+		/*
+		 * Regular Execution Job
+		 * check if the startAt parameter is present, which might cause job to be executed in future
+		 * 
+		 * if not present then use created setting from job level
+		 */
+		DateTime temporalCoverageEndDate = getTemporalCoverageEndDate_regularJob(job);
+		
+		String duration = getAssociatedDuration(job, subsetDefinition);
+		String offset = getAssociatedDurationOffset(job, subsetDefinition);
+		
+		DateTime temporalCoverageStartDate = computeTemporalCoverageStartDate(temporalCoverageEndDate, duration, offset);
+		
+		data_cloned = setTemporalCoverage(data_cloned, temporalCoverageStartDate, temporalCoverageEndDate);
+		
+        return data_cloned;
+	};
+
+	default DateTime getTemporalCoverageEndDate_regularJob(WacodisJobDefinition job) {		
+		DateTime startAt = job.getExecution().getStartAt();
+		
+		if(startAt != null) {
+			return startAt;
+		}
+		else {
+			return job.getCreated();
+		}
+	};
+
+	default JobDataMap configureFirstDataQueryPeriod_singleExecutionJob(WacodisJobDefinition job, JobDataMap data_cloned,
+			AbstractSubsetDefinition subsetDefinition, SingleJobExecutionEvent event) {
+
+		/*
+		 * Single Execution Job
+		 * Event --> temporalCoverageEndDate  --> ist immer das Enddatum der Betrachtung --> duration backwards from that date
+		 */
+		DateTime temporalCoverageEndDate = event.getTemporalCoverageEndDate();
+		
+		String duration = getAssociatedDuration(job, subsetDefinition);
+		String offset = getAssociatedDurationOffset(job, subsetDefinition);
+		
+		DateTime temporalCoverageStartDate = computeTemporalCoverageStartDate(temporalCoverageEndDate, duration, offset);
+		
+		data_cloned = setTemporalCoverage(data_cloned, temporalCoverageStartDate, temporalCoverageEndDate);
+		
+        return data_cloned;
+	};
+
+	default JobDataMap setTemporalCoverage(JobDataMap data_cloned, DateTime temporalCoverageStartDate,
+			DateTime temporalCoverageEndDate) {
+		data_cloned.put(TemporalCoverageConstants.START_DATE, temporalCoverageStartDate);
+		data_cloned.put(TemporalCoverageConstants.END_DATE, temporalCoverageEndDate);
+		
+		return data_cloned;
+	};
+
+	default DateTime computeTemporalCoverageStartDate(DateTime temporalCoverageEndDate, String duration,
+			String offset) {
+		DateTime temporalCoverageStartDate;
+		Period durationPeriod = Period.parse(duration, ISOPeriodFormat.standard());        
+
+        temporalCoverageStartDate = temporalCoverageEndDate.withPeriodAdded(durationPeriod, -1);
+        
+        // apply additional offset if present
+        if(offset != null) {
+        	Period offsetPeriod = Period.parse(offset, ISOPeriodFormat.standard());
+
+            temporalCoverageStartDate = temporalCoverageStartDate.withPeriodAdded(offsetPeriod, -1);
+        }
+        
+        return temporalCoverageStartDate;
+	};
+
+	default String getAssociatedDurationOffset(WacodisJobDefinition job, AbstractSubsetDefinition subsetDefinition) {
+		// use input specific details if available; else use job level details
+		String offset = null;
+		AbstractSubsetDefinitionTemporalCoverage temporalCoverage_subsetDef = subsetDefinition.getTemporalCoverage();
+		WacodisJobDefinitionTemporalCoverage temporalCoverage_job = job.getTemporalCoverage();
+		if (temporalCoverage_subsetDef != null) {
+			offset = temporalCoverage_subsetDef.getOffset();
+		} else {
+			offset = temporalCoverage_job.getOffset();
+		}
+
+		return offset;
+	};
+
+	default String getAssociatedDuration(WacodisJobDefinition job, AbstractSubsetDefinition subsetDefinition) {
+		// use input specific details if available; else use job level details
+		String duration = null;
+		AbstractSubsetDefinitionTemporalCoverage temporalCoverage_subsetDef = subsetDefinition.getTemporalCoverage();
+		WacodisJobDefinitionTemporalCoverage temporalCoverage_job = job.getTemporalCoverage();
+		if (temporalCoverage_subsetDef != null) {
+			duration = temporalCoverage_subsetDef.getDuration();
+		} else {
+			duration = temporalCoverage_job.getDuration();
+		}
+
+		return duration;
+	};
+
+	/**
      * Pack business logic data into JobDataMap's data package and build new JobDetail
      *
      * @param job  - Instance of a new Job
---
+++ datasource-observer-core/src/main/java/de/wacodis/observer/core/TemporalCoverageConstants.java
@@ -0,0 +1,8 @@
+package de.wacodis.observer.core;
+
+public class TemporalCoverageConstants {
+
+	public static final String START_DATE = "TEMPORAL_COVERAGE_START_DATE";
+	public static final String END_DATE = "TEMPORAL_COVERAGE_END_DATE";
+	
+}
